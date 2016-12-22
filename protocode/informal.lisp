72261c0b39cfa972bcb2de39f229d1e515b6aa36
Baggers <chris.bagley@gmail.com>
2016-12-22 20:35:49 +0100

    first sign of first class funcs in CEPL

--- protocode/informal.lisp
+++ protocode/informal.lisp
@@ -6,3 +6,7 @@
 
 (defun-g int-to-int-dispatcher ((fn (function (:int) :int)) (a :int))
   (funcall fn a))
+
+
+(defun-g innocent ((a :int))
+  (v! 1 2 3 (int-to-int-dispatcher #'(doubler :int) a)))
