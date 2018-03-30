14098650b8435d49433f2e0ed036474f959048bc
cbaggers <chris.bagley@gmail.com>
2018-03-30 20:27:02 +0200

    bake-uniforms test that would have caught bug from other day
    baking was using &uniforms instead of uniform and we didnt know as we
    only had function args in our bake tests

--- tests/bake.lisp
+++ tests/bake.lisp
@@ -12,6 +12,15 @@
 (defpipeline-g bake-pipeline-0 (:points)
   :fragment (bake-frag-0 :vec2))
 
+(defun-g bake-frag-1 ((uv :vec2)
+                      &uniform
+                      (foo (function (:vec2) :vec4))
+                      (blarp :float))
+  (* (funcall foo uv) blarp))
+
+(defpipeline-g bake-pipeline-1 (:points)
+  :fragment (bake-frag-1 :vec2))
+
 ;;------------------------------------------------------------
 ;; helper functions
 
@@ -43,6 +52,22 @@
                    (pipeline (bake-uniforms #'bake-pipeline-0 :foo glam)))
         (with-fbo-bound (fbo)
           (map-g (the function pipeline) bs))
+        (let* ((data (car (pull-g (attachment fbo 0)))))
+          (loop :for vec :in data :do
+             (is (and (= (x vec) (y vec))
+                      (= (z vec) (w vec))))))))))
+
+(def-test bake-2 (:suite cepl.pipelines)
+  (ensure-cepl
+    (let ((glam (lambda-g ((val :vec2))
+                  (s~ val :xxyy))))
+      (with-free* ((fbo (make-fbo '(0 :dimensions (4 4) :element-type :vec4)))
+                   (bs (make-buffer-stream nil :primitive :points))
+                   ;;
+                   (pipeline (bake-uniforms #'bake-pipeline-1 :foo glam)))
+        (with-fbo-bound (fbo)
+          (map-g (the function pipeline) bs
+                 :blarp 2f0))
         (let* ((data (car (pull-g (attachment fbo 0)))))
           (loop :for vec :in data :do
              (is (and (= (x vec) (y vec))
